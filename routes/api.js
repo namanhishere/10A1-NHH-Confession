646f384d92fea8eb574808fd5e4933b0b48c3ede
namanhishere <namanhishere0208@gmail.com>
2020-08-27 19:39:05 +0700

    test

--- routes/api.js
+++ routes/api.js
@@ -4,18 +4,24 @@ const superagent = require("superagent")
 var database = require("../function/database").mysql
 
 router.post('/post', async function(req, res, next) {
-    da
-    let confession_content = req.query.content
-    let content = `Confession số 1:\n${confession_content}\n\n\n//Đăng tải confession tại:https://nhh-10a1-confession.herokuapp.com/`
-    superagent.post("https://graph.facebook.com/1378190169043603/feed").send({
-        message:content,
-        access_token: "EAAkgNv6skioBAGJcsx7HhHJmqIA6oZBEZBw0DUW2R3WCBAIf1b9GVGDVyWpQZBChKeQUSf6s8EjI0kHeH41n9y0UuPN6DBLe6NZCKFAVmetZB76uP0TYpec7mFZAuBcPIpTs4OGCLxMWjYiOXQQMi8y1gVkvgrrM5XEG9NcUytbV9mRXE9ZC6th"
-    }).end((err,data) =>{
-        let prase = JSON.parse(data.text)
-        res.json({
-            id : prase.id
+    database.query("SELECT * FROM thong_tin_he_thong WHERE rootchar = 1", function (er,d) {
+        if(er) return res.json(er)
+        let confess_num = parseInt(d[0].confession_hien_tai) + 1
+        console.log(d[0].access_token_hien_tai)
+        let confession_content = req.query.content
+        database.query(`UPDATE thong_tin_he_thong SET confession_hien_tai = ${confess_num} WHERE rootchar = 1`)
+        let content = `#Confession_số_${confess_num}:\n${confession_content}\n\n\n//Đăng tải confession của bạn tại:https://nhh-10a1-confession.herokuapp.com/\n\n#10a1 #confession #chuyen_tuoi_hong`
+        superagent.post("https://graph.facebook.com/225864295529972/feed").send({
+            message:content,
+            access_token: d[0].access_token_hien_tai
+        }).end((err,data) =>{
+            let prase = JSON.parse(data.text)
+            res.json({
+                id : prase.id
+            })
         })
     })
+   
     
 
 
