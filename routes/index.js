1974eb858b28f019a157411f6a872a53995caf61
Bé Na Cute <53372786+namanhishere@users.noreply.github.com>
2020-09-23 07:00:07 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/nhac', function(req, res, next) {
+  res.redirect('https://google.com');
+});
+
+
 module.exports = router;
